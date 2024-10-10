d45eeb52a430f803c6fa7af4c17e7fbda01f26a6
ielbusmente <ieljbusmente@gmail.com>
2024-10-11 00:10:00 +0800

    Handle index out of bounds - enough for the day, i m tired

--- Program.cs
+++ Program.cs
@@ -5,4 +5,13 @@ string[] students = ["Sophia", "Nicolas", "Zahirah", "Jeong"];
 
 int studentCount = students.Length;
 
-Console.WriteLine("The final name is: " + students[studentCount]);
+try
+{
+    // okay, so to use the debugger - Run and Debug Ctrl + Shift + D, you open the Solution Explorer under the Explorer Tab hhhhhh
+    Console.WriteLine("The final name is: " + students[studentCount]);
+}
+catch (Exception e)
+{
+    Console.WriteLine("Error: " + e.ToString());
+}
+
